f615a22024289aa91ef9fe1c535d49edcb0119ff
miraculixx <miraculixx@gmx.ch>
2017-10-29 16:10:46 +0100

    add command help and options for custom class and keysfile

--- stackable/encryptkeys.py
+++ stackable/encryptkeys.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 from uuid import uuid4
@@ -7,21 +8,21 @@ import yaml
 from .crypto import AESCipher
 
 
-def siteenv(site=None, env_class='EnvSettings_Local', api_password=None,
+def siteenv(site=None, envclass='EnvSettings_Local', api_password=None,
             keysfile=None, plain=False, env_var=None):
     """
     print the site environment variables for deployment
     """
     site = site or os.path.basename(os.path.dirname(__file__))
-    keysfile = os.path.expanduser('~/.stackable/keys.yml')
+    keysfile = keysfile or os.path.expanduser('~/.stackable/keys.yml')
     print "Creating variables for %s from %s" % (site, keysfile)
     api_password = "%s" % uuid4() or os.environ.get('ENV_APIKEY_DECRYPT')
     with open(keysfile, 'r') as f:
         # load cleartext keys and config settings from yml file
         keys = yaml.load(f)
-        e_settings = keys.get(env_class, None)
+        e_settings = keys.get(envclass, None)
         if e_settings is None:
-            print "[WARN] Cannot find any keys for %s" % env_class
+            print "[WARN] Cannot find any keys for %s" % envclass
             exit()
         if env_var is not None:
             # prints the variable value
@@ -29,12 +30,12 @@ def siteenv(site=None, env_class='EnvSettings_Local', api_password=None,
             exit()
         # create encrypted key file per environment
         aes = AESCipher(api_password)
-        keyenvvar = '%s_KEYS' % env_class
+        keyenvvar = '%s_KEYS' % envclass
         cleartext = json.dumps(e_settings)
         ciphertext = aes.encrypt(cleartext)
     envvars = [
-        'DJANGO_CONFIGURATION=%s' % env_class,
-        'DJANGO_CONFIGURATION_TEST=%s' % env_class,
+        'DJANGO_CONFIGURATION=%s' % envclass,
+        'DJANGO_CONFIGURATION_TEST=%s' % envclass,
         'ENV_APIKEY_DECRYPT=%s' % api_password,
         '%s=%s' % (keyenvvar, ciphertext.replace('\n', '')),
     ]
@@ -46,6 +47,11 @@ def siteenv(site=None, env_class='EnvSettings_Local', api_password=None,
     else:
         print "\n".join(envvars)
 
+parser = argparse.ArgumentParser(description='stackable key encryption')
+parser.add_argument('--keysfile', help='/path/to/keys.yml, defaults to ~/.stackable/keys.yml')
+parser.add_argument('--envclass', help='environment class name, defaults to EnvSettings_Local')
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    siteenv()
+    siteenv(**vars(args))
